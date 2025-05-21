ac8434651d44758a97a3f64fdba56db998f7d100
TheBlackParrot <theblackparrot0@gmail.com>
2025-05-21 11:15:43 -0500

    actually fix the funny lighting bug on 1.40.3 this time

---
+++ MenuCoverColors/Patches/ForceEnvironmentColorsOnMapExit.cs
@@ -0,0 +1,44 @@
+﻿#if V1_40_3
+using HarmonyLib;
+using UnityEngine;
+
+namespace MenuCoverColors.Patches;
+
+// this works around a bug in 1.40.3+ where boost lights will effect menu lighting when maps use boost lights at any point
+// the game's default "light color" in the menu is #808080FF (unity's color.gray is #7F7F7F but whatever)
+// https://github.com/Meivyn/BeatSaberBugs/issues/20
+
+[HarmonyPatch]
+internal class ForceEnvironmentColorsOnMapExitPatch
+{
+    private static ColorScheme PatchColors(ColorSchemeSO schemeObj)
+    {
+        return new ColorScheme(schemeObj)
+        {
+            _colorSchemeId = "ActiveColorSchemeEnvironmentPatchedForMenuCoverColors",
+            _colorSchemeNameLocalizationKey = "ActiveColorSchemeEnvironmentPatchedForMenuCoverColors",
+            _useNonLocalizedName = true,
+            _nonLocalizedName = "ActiveColorSchemeEnvironmentPatchedForMenuCoverColors",
+            _isEditable = false,
+            _environmentColor0 = Color.gray,
+            _environmentColor1 = Color.gray,
+            _supportsEnvironmentColorBoost = false
+        };
+    }
+
+    [HarmonyPatch(typeof(GameplayLevelSceneTransitionEvents), "HandleStandardLevelDidFinish")]
+    [HarmonyPrefix]
+    // ReSharper disable once InconsistentNaming
+    public static bool ForceEnvironmentColorsOnMapExit(GameplayLevelSceneTransitionEvents __instance)
+    {
+        ColorSchemeSO schemeObj = ScriptableObject.CreateInstance<ColorSchemeSO>();
+        schemeObj._colorScheme = __instance._standardLevelScenesTransitionSetupData.colorScheme;
+        
+        ColorScheme patched = PatchColors(schemeObj);
+        
+        __instance._standardLevelScenesTransitionSetupData.usingOverrideColorScheme = true;
+        __instance._standardLevelScenesTransitionSetupData.colorScheme = patched;
+        return true;
+    }
+}
+#endif

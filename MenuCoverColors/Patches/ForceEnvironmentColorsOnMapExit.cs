3c8bf82a79624233b50d70647736d98547e26c38
TheBlackParrot <theblackparrot0@gmail.com>
2025-05-29 01:41:05 -0500

    surely this time :clueless:

--- MenuCoverColors/Patches/ForceEnvironmentColorsOnMapExit.cs
+++ MenuCoverColors/Patches/ForceEnvironmentColorsOnMapExit.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 namespace MenuCoverColors.Patches;
 
 // this works around a bug in 1.40.3+ where boost lights will effect menu lighting when maps use boost lights at any point
-// the game's default "light color" in the menu is #808080FF (unity's color.gray is #7F7F7F but whatever)
 // https://github.com/Meivyn/BeatSaberBugs/issues/20
 
 [HarmonyPatch]
 internal class ForceEnvironmentColorsOnMapExitPatch
 {
+    private static readonly Color DefaultColor = new(0.502f, 0.502f, 0.502f, 1.0f);
     private static ColorScheme PatchColors(ColorSchemeSO schemeObj)
     {
         return new ColorScheme(schemeObj)
@@ -20,9 +20,14 @@ internal class ForceEnvironmentColorsOnMapExitPatch
             _useNonLocalizedName = true,
             _nonLocalizedName = "ActiveColorSchemeEnvironmentPatchedForMenuCoverColors",
             _isEditable = false,
-            _environmentColor0 = Color.gray,
-            _environmentColor1 = Color.gray,
-            _supportsEnvironmentColorBoost = false
+            _environmentColor0 = DefaultColor,
+            _environmentColor1 = DefaultColor,
+            _environmentColor0Boost = DefaultColor,
+            _environmentColor1Boost = DefaultColor,
+            _environmentColorWBoost = DefaultColor,
+            _environmentColorW = DefaultColor,
+            _overrideLights = true,
+            _supportsEnvironmentColorBoost = true
         };
     }
 
